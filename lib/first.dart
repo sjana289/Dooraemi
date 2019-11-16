b72e8128dcbd9d0f37d4767ce051bc6ce1266859
mainak679 <56960568+mainak679@users.noreply.github.com>
2019-11-16 09:39:44 +0800

    Update first.dart

--- lib/first.dart
+++ lib/first.dart
@@ -1,14 +1,49 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Center(
-              child:Text
-                    ("Hi!! I'm Dooraemi..."),
-          ),
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue,
+        appBar: AppBar(
+          title: Text("Dooraemi!!"),
+          backgroundColor: Colors.green,
+        ),
+        drawer: Drawer(
+          //listview
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                  child: Text("Drawer header"),
+                decoration: BoxDecoration(
+                  color: Colors.lightGreen,
+                ),
+              ),
+              ListTile(
+                title:Text("Recent"),
+               /* onTap: (){
+                  Navigator.pop(context);
+                },*/
+              ),
+              ListTile(
+                title:Text("History"),
+                /* onTap: (){
+                  Navigator.pop(context);
+                },*/
+              ),
+              ListTile(
+                title:Text("DND"),
+                /* onTap: (){
+                  Navigator.pop(context);
+                },*/
+              )
+
+            ]
+
+          )
         ),
       ),
     ),
-);
+  );
+}
